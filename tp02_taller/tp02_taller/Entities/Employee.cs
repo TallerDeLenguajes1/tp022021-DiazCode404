922f134995b1fac68a1635aa881eb5f8e2f390ba
DerAufhocker <exequieldiaz404@gmail.com>
2021-09-04 16:50:00 -0300

    .

---
+++ tp02_taller/tp02_taller/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tp02_taller.Entities
+{
+    public class Employee
+    {
+    }
+}
